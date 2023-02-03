cafcbf32fa53f50a49581cdfea910758726222a9
sala7khaled <sala7khaled7@gmail.com>
2023-02-03 02:11:13 +0200

    first commit

---
+++ FilmKu/Modules/Home/View/Cell/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  FilmKu
+//
+//  Created by Salah Khaled on 03/02/2023.
+//  Copyright © 2023 Salah Khaled. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ FilmKu/Utilites/ReuseableViews/Alert/AlertController.swift
@@ -0,0 +1,9 @@
+//
+//  AlertController.swift
+//  FilmKu
+//
+//  Created by Salah Khaled on 03/02/2023.
+//  Copyright © 2023 Salah Khaled. All rights reserved.
+//
+
+import Foundation
